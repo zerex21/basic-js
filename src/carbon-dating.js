ea8b6fdbda06a36e3d4f0533f22f89d1237491e7
oleg <58697249+zerex21@users.noreply.github.com>
2021-02-21 17:56:39 +0300

    change

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,11 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity ) {
+ 
+  if (typeof sampleActivity === 'string' && Number.isNaN(+sampleActivity))  return false;
+  if (typeof sampleActivity !== 'string') return false;
+  sampleActivity = +sampleActivity;
+
+  return Math.ceil( Math.log(MODERN_ACTIVITY / (+sampleActivity)) / (0.693 / HALF_LIFE_PERIOD) );
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,15 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
+module.exports = function countCats(backyard) {
+ // throw 'Not implemented';
   // remove line with error and write your code here
+   let res = 0;
+  backyard.forEach(item => {
+    item.forEach(item2 => {
+      if (item2 === '^^') {
+        res++;
+      }
+    })    
+  });
+  return res; 
+
 };
+
